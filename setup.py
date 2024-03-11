e9c7c9829edc9536bdbc1cc2664daa2e052ab9bf
Petar Crnjak <30388414+PCrnjak@users.noreply.github.com>
2024-03-11 14:20:45 +0100

    debug disable

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.17'
+VERSION = '1.18'
 DESCRIPTION = 'Python module for Spectral BLDC motor controllers'
 
 # Setting up
